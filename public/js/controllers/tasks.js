c8c4facaf42c246e07e8a420cb4d94785d7db686
evanjmg <evanjmg@gmail.com>
2015-09-18 02:11:42 +0100

    finished most of show page

--- public/js/controllers/tasks.js
+++ public/js/controllers/tasks.js
@@ -24,7 +24,7 @@ function TasksController(Flash, User, Task, $state, $stateParams, TokenService, 
 
   if (TokenService.isAuthed()) {
     self.currentUser = TokenService.parseJwt();
-  
+    
   }
 
   // Setup defaults
@@ -32,53 +32,55 @@ function TasksController(Flash, User, Task, $state, $stateParams, TokenService, 
   self.all = Task.query();
   self.showCamera       = false;
   self.selectUsersPage  = false;
- 
+  
   self.task.completion = self.task.completion || {};
   if ($stateParams.query) $scope.query = $stateParams.query
 
-  if ($stateParams.id) {
-    self.task = Task.get({id: $stateParams.id}, function (response) {
-      self.task = response;
-     if (self.task.created_at) {
-       self.format_created_at = moment(self.task.created_at).startOf('hour').fromNow(); 
-     }
-      
-    });
-  }
-
-  if ($('canvas').length !== 0 || $('#shareChallengeButton').css('display') !== 'none') { 
-    Geo.locate(function (data) {
-      self.lat =  data.coords.latitude;
-      self.lon =  data.coords.longitude;
-    });
-  }
-  
-  User.query(function (response) {
-    var i=0; for (i;i < response.length;i++) {
-      if (response[i].id  == self.currentUser.id) {
-        var index = i;
+    if ($stateParams.id) {
+      self.task = Task.get({id: $stateParams.id}, function (response) {
+        self.task = response;
+        if (self.task.created_at) {
+         self.format_created_at = moment(self.task.created_at).startOf('hour').fromNow(); 
+       }
+       if (self.task.completion.time_completed) {
+        self.format_time_completed = moment(self.task.completion.time_completed).startOf('hour').fromNow();
       }
-    }
-    response.splice(index, 1);
-    self.allUsers = response;
-    console.log(response[0].id);
-  });
-
-  self.startCamera = function(){
-    $scope.showCamera = true;
-  }
-
-  $scope.$watch('showCamera', function(showCamera){
-    if (showCamera) {
       
-      setTimeout(function(){
-        PhotoUpload.startup();
-      }, 200);
+    });
     }
-  });
-  self.openMap = function () {
-    $('.map-container').show();
 
+    if ($('canvas').length !== 0 || $('#shareChallengeButton').css('display') !== 'none') { 
+      Geo.locate(function (data) {
+        self.lat =  data.coords.latitude;
+        self.lon =  data.coords.longitude;
+      });
+    }
+    
+    User.query(function (response) {
+      var i=0; for (i;i < response.length;i++) {
+        if (response[i].id  == self.currentUser.id) {
+          var index = i;
+        }
+      }
+      response.splice(index, 1);
+      self.allUsers = response;
+      console.log(response[0].id);
+    });
+
+    self.startCamera = function(){
+      $scope.showCamera = true;
+    }
+
+    $scope.$watch('showCamera', function(showCamera){
+      if (showCamera) {
+        
+        setTimeout(function(){
+          PhotoUpload.startup();
+        }, 200);
+      }
+    });
+    self.openMap = function () {
+      $('.map-container').show();
 
       initializeMap(function (latLon, map) {
         var marker = new google.maps.Marker({
@@ -87,152 +89,144 @@ function TasksController(Flash, User, Task, $state, $stateParams, TokenService, 
           visible: true
         });
         marker.setMap(map);
-        
+
         var infowindow = new google.maps.InfoWindow({
           content:self.task.location.name
-          });
+        });
 
         infowindow.open(map,marker);
-    
+        
       });
-      // var myLatLng = {lat: parseFloat(self.task.location.lat), lng: parseFloat(self.task.location.lon) }
-      
-      // var map = new google.maps.Map(document.getElementById('map'), {
-      //   zoom: 14,
-      //   center: myLatLng
-      // });
-
-   
-  $('.card-bottom').hide();
+      $('.card-bottom').hide();
     }
-   
-  
-  self.closeMap = function () {
-    $('.map-container').hide();
-    $('.card-bottom').show();
-  }
-  self.takePicture = function() {
-    PhotoUpload.takepicture();
-  }
-
-  self.decideTask = function (bool) {
-    if (bool) {
-      $state.go('completeTask', { id: $stateParams.id });
-    } else {
     
+    
+    self.closeMap = function () {
+      $('.map-container').hide();
+      $('.card-bottom').show();
+    }
+    self.takePicture = function() {
+      PhotoUpload.takepicture();
+    }
 
-    Task.reject({ id: $stateParams.id }, function (response) {
-   
-      Flash.create('success', "<h4> Rejected "+ self.task._creator.name +"'s challenge</h4>", 'custom-class');
-    });
-    $state.go('myTasks');
-    }   
-  }
-  self.acceptResponse = function () {
-    Task.acceptResponse({ id: $stateParams.id}, function (response) {
-      
-      $state.go('createdTasks');
-      Flash.create('success', "<h4> Successfully accepted "+ self.task._tagged_member.name +"'s response</h4>", 'custom-class');
-    });
-  }
-  self.rejectResponse = function () {
-    Task.rejectResponse({ id: $stateParams.id}, function (response) {
-     
-      $state.go('createdTasks');
-      Flash.create('warning', "<h4> Rejected "+ self.task._tagged_member.name +"'s response</h4>", 'custom-class');
-    })
-  }
-  self.completeTask = function () {
+    self.decideTask = function (bool) {
+      if (bool) {
+        $state.go('completeTask', { id: $stateParams.id });
+      } else {
+        
 
-    Task.complete({ task: self.task, lat: self.lat, lon: self.lon }, function (response) {
-      $state.go('myTasks');
+        Task.reject({ id: $stateParams.id }, function (response) {
+         
+          Flash.create('success', "<h4> Rejected "+ self.task._creator.name +"'s challenge</h4>", 'custom-class');
+        });
+        $state.go('myTasks');
+      }   
+    }
+    self.acceptResponse = function () {
+      Task.acceptResponse({ id: $stateParams.id}, function (response) {
+        
+        $state.go('createdTasks');
+        Flash.create('success', "<h4> Successfully accepted "+ self.task._tagged_member.name +"'s response</h4>", 'custom-class');
+      });
+    }
+    self.rejectResponse = function () {
+      Task.rejectResponse({ id: $stateParams.id}, function (response) {
+       
+        $state.go('createdTasks');
+        Flash.create('warning', "<h4> Rejected "+ self.task._tagged_member.name +"'s response</h4>", 'custom-class');
+      })
+    }
+    self.completeTask = function () {
+
+      Task.complete({ task: self.task, lat: self.lat, lon: self.lon }, function (response) {
+        $state.go('myTasks');
 
       // $('.taskInvitesHeader').append();
       Flash.create('success', "<h4> You responded to "+ self.task._creator.name +"'s challenge. Awaiting confirmation.</h4>", 'custom-class');
     })
-  }
- self.copyTask = function (user) {
-  self.task._id = undefined;
-  self.task._creator = self.currentUser.id; 
-  self.task._tagged_member = user.id;
-  self.task.completed = null;
+    }
+    self.copyTask = function (user) {
+      self.task._id = undefined;
+      self.task._creator = self.currentUser.id; 
+      self.task._tagged_member = user.id;
+      self.task.completed = null;
 
-  Task.copy({ task: self.task, lat: self.lat, lon: self.lon }, function (response){
-    
-    $state.go('createdTasks');
-    Flash.create('success', "<h4> You shared this challenge with "+ self.task._tagged_member.name +". Awaiting their response.</h4>", 'custom-class');
-  });
+      Task.copy({ task: self.task, lat: self.lat, lon: self.lon }, function (response){
+        
+        $state.go('createdTasks');
+        Flash.create('success', "<h4> You shared this challenge with "+ self.task._tagged_member.name +". Awaiting their response.</h4>", 'custom-class');
+      });
 
 
- }
-  self.upload = function () {
-    if (!self.file) {
-      PhotoUpload.upload(null, function(img_url){
-     if ($('#createTaskPage').length > 0) {
-        self.task.img_url = img_url;
+    }
+    self.upload = function () {
+      if (!self.file) {
+        PhotoUpload.upload(null, function(img_url){
+         if ($('#createTaskPage').length > 0) {
+          self.task.img_url = img_url;
+
+        } else {
+          self.task.completion.img_url = img_url; 
+        }
+      });
+        $scope.showCamera = false;
 
       } else {
-        self.task.completion.img_url = img_url; 
-      }
-      });
-      $scope.showCamera = false;
+        PhotoUpload.upload(self.file, function(img_url){
 
-    } else {
-      PhotoUpload.upload(self.file, function(img_url){
-
-      if ($('#createTaskPage').length > 0) {
-         self.task.img_url = img_url;
-       
-       } else {
-         self.task.completion.img_url = img_url; 
-       }
-      });
-    }
-  }
-
-  self.updateTask = function () {
-    self.task._creator = self.task._creator.id;
-    self.task._tagged_member = self.task._tagged_member.id;
-    Task.update(self.task, function (response) {
-      $state.go('showTask', { team_id: $stateParams.team_id, id: $stateParams.id});
-    })
-  }
-
-  self.retakePhoto = function () {
-    $('.cameraOptionButtons').hide();
-    $('#canvas').hide();
-    $('#video').show();
-  }
-
-  self.backToTeam = function () {
-    $state.go('showTeam', { id: $stateParams.team_id });
-  }
-
- 
-  self.showUsersPage = function () {
-    self.selectUsersPage = true;
-  }
-
-  function initializeMap (callback) {
-    var lon  = parseFloat(self.task.location.lon);
-    var lat = parseFloat(self.task.location.lat);
-    var name = self.task.location.name;
-     var map = new google.maps.Map(document.getElementById('map'), {
-         zoom: 14,
-         center: {lat: lat , lng: lon },
-         styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.country","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"geometry.fill","stylers":[{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"labels","stylers":[{"hue":"#ffe500"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"},{"visibility":"on"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.landcover","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry.stroke","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"labels.text.fill","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"labels.text.stroke","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.attraction","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.place_of_worship","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45},{"visibility":"on"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"transit.station","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"transit.station.airport","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#9bdffb"},{"visibility":"on"}]}]
+          if ($('#createTaskPage').length > 0) {
+           self.task.img_url = img_url;
+           
+         } else {
+           self.task.completion.img_url = img_url; 
+         }
        });
- 
-   var latLon ={lat: lat , lng: lon }
-    callback(latLon, map);
+      }
+    }
+
+    self.updateTask = function () {
+      self.task._creator = self.task._creator.id;
+      self.task._tagged_member = self.task._tagged_member.id;
+      Task.update(self.task, function (response) {
+        $state.go('showTask', { team_id: $stateParams.team_id, id: $stateParams.id});
+      })
+    }
+
+    self.retakePhoto = function () {
+      $('.cameraOptionButtons').hide();
+      $('#canvas').hide();
+      $('#video').show();
+    }
+
+    self.backToTeam = function () {
+      $state.go('showTeam', { id: $stateParams.team_id });
+    }
+
+    
+    self.showUsersPage = function () {
+      self.selectUsersPage = true;
+    }
+
+    function initializeMap (callback) {
+      var lon  = parseFloat(self.task.location.lon);
+      var lat = parseFloat(self.task.location.lat);
+      var name = self.task.location.name;
+      var map = new google.maps.Map(document.getElementById('map'), {
+       zoom: 14,
+       center: {lat: lat , lng: lon },
+       styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.country","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"geometry.fill","stylers":[{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"labels","stylers":[{"hue":"#ffe500"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"},{"visibility":"on"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.landcover","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry.stroke","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"labels.text.fill","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"labels.text.stroke","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.attraction","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.place_of_worship","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45},{"visibility":"on"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"transit.station","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"transit.station.airport","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#9bdffb"},{"visibility":"on"}]}]
+     });
+
+var latLon ={lat: lat , lng: lon }
+callback(latLon, map);
     // window.setTimeout(function (){
-   
+     
     // },1200 );
-  }
-  self.createTask = function (recipient) { 
-    self.task.minutes = $('#input-number').val();
-    self.task._creator = self.currentUser.id;
-    self.task._tagged_member = recipient.id;
+}
+self.createTask = function (recipient) { 
+  self.task.minutes = $('#input-number').val();
+  self.task._creator = self.currentUser.id;
+  self.task._tagged_member = recipient.id;
     // self.task.team_id = $stateParams.team_id;
 
     Task.save({ task: self.task, lat: self.lat, lon: self.lon  }, function (response) {
@@ -247,7 +241,7 @@ function TasksController(Flash, User, Task, $state, $stateParams, TokenService, 
 
   self.showTask = function () {
     Task.get({id: $state.params.id},function (response){
- 
+     
       self.task = response;
     } )
   } 
