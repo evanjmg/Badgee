1a10d761c95ab7fd0533a272be3738232a2f531d
evanjmg <evanjmg@gmail.com>
2015-09-26 20:44:57 +0100

    fixed profile header

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,14 +8,27 @@ module.exports = function(grunt) {
         }
       }
     },
+     server: {
+      port: 5000,
+      base: './public'
+    },
     watch: {
+     options: {
+           livereload: true,
+         },
+     files: '**/*.html',
       css: {
         files: '**/*.scss',
         tasks: ['sass']
       }
     }
   });
+  
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('server', 'Start a custom web server', function() {
+    grunt.log.writeln('Started web server on port 5000');
+    require('./app.js').listen(5000);
+  });
   grunt.registerTask('default',['watch']);
 }
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express    = require("express");
-var app        = express();
+var app        = module.exports = express();
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require('mongoose');
